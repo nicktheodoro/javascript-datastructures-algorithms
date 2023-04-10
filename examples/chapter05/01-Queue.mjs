6530a6347d4c63612d96e486c7eaad157b687d7b
nicktheodoro <nicolastheodoro@gmail.com>
2023-04-10 07:44:56 -0300

    chapter05 - examples 01, 02

---
+++ examples/chapter05/01-Queue.mjs
@@ -0,0 +1,14 @@
+import Queue from "../../src/data-structures/queue.mjs";
+
+const queue = new Queue();
+console.log(queue.isEmpty()); // outputs true
+queue.enqueue('John');
+queue.enqueue('Jack');
+console.log(queue.toString()); // John,Jack
+queue.enqueue('Camila');
+console.log(queue.toString()); // John,Jack,Camila
+console.log(queue.size()); // outputs 3
+console.log(queue.isEmpty()); // outputs false
+queue.dequeue(); // remove John
+queue.dequeue(); // remove Jack
+console.log(queue.toString()); // Camila
